c044e697011cbf4ce60f8c84c433aec4ac5ee09c
NoNameCoder2806 <hoangvien2806@gmail.com>
2025-12-09 19:27:26 -0800

    Coding the foundation of the program

---
+++ LinkedList.h
@@ -0,0 +1,14 @@
+// COMSC-210 | Final Exam 2 | Dat Hoang Vien
+// IDE used: Visual Studio Code
+
+// Libraries
+#include <iostream>
+#include <string>
+using namespace std;
+
+// Linked List's node
+struct node
+{
+    // Members
+
+};
---
+++ main.cpp
@@ -0,0 +1,23 @@
+// COMSC-210 | Final Exam 2 | Dat Hoang Vien
+// IDE used: Visual Studio Code
+
+// Libraries
+#include <iostream>
+using namespace std;
+
+// Constants
+    // Customer names
+    string names[20] = {
+        "Alice", "Bob", "Charlie", "Dana", "Eli", "Fiona", "George", "Hannah", 
+        "Ian", "Jill", "Kevin", "Luna", "Mason", "Nina", "Oscar", "Paula", 
+        "Quinn", "Ryan", "Sophia", "Tyler"
+    };
+
+    // Customer drinks
+    
+
+// Main function
+int main()
+{
+    return 0;
+}

72250d9e43cd8f5cb88f3faef53058419d86fcd5
NoNameCoder2806 <hoangvien2806@gmail.com>
2025-12-09 20:17:38 -0800

    Simulating the Linked List

--- main.cpp
+++ main.cpp
@@ -27,7 +27,7 @@ const string COFFEE[10] = {
 };          // Coffee
 
 // Functions prototypes
-void simulateCoffeeBooth(Node* head, Node* tail);
+void simulateCoffeeBooth(Node*& head, Node*& tail);
 
 // Main function
 int main()
@@ -90,12 +90,50 @@ int main()
         - tail: the Node at the tail of the Linked List
     Return: none
 */
-void simulateCoffeeBooth(Node* head, Node* tail)
+void simulateCoffeeBooth(Node*& head, Node*& tail)
 {
-    // Display a message
-    cout << "    " << head->name << " is served!" << endl;
-    
-    // Remove the first customer (customer is served)
-    Node* temp = head;
-    temp = temp->next;
+    // Check whether the queue is empty
+    // If the queue if NOT empty
+    if (head != nullptr)
+    {
+        // Then the first customer is always served
+        // Display a message
+        cout << "    " << head->name << " is served!" << endl;
+
+        // Remove the first customer (customer is served)
+        Node* temp = head;
+        head = head->next;
+        delete temp;
+    }
+
+    // And there's always a 50% chance a Customer joins the queue
+    int chance = rand() % 100 + 1;          // Chance ranges rom 1 -> 100
+
+    // Compare the chance
+    if (chance <= JOIN_QUEUE)
+    {
+        // Create a new Node to store the data
+        Node* newNode = new Node;
+
+        // Populate the data
+        newNode->name = NAMES[rand() % NAMES_SIZE];
+        newNode->order = COFFEE[rand() % COFFEE_SIZE];
+
+        // Add the Node into the Linked List
+        // If the Linked List is empty
+        if (head == nullptr)
+        {
+            // We let the newNode be both the head and tail
+            head = tail = newNode;
+            newNode->next = nullptr;
+        }
+        // Otherwise, if the Linked List is not empty
+        else
+        {
+            // We add to the tail of the Linked List and set it to be the new tail
+            tail->next = newNode;
+            newNode->next = nullptr;
+            tail = newNode;
+        }
+    }
 }
